21293a139e2f3c762d1467aa3729ec15ed94db39
Vitalie Andries <vitalie.andries@gmail.com>
2019-01-21 14:17:52 +0200

    amplify init

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Amplify from 'aws-amplify';
+import aww_exports from './aws-exports';
+
+Amplify.configure(aww_exports);
+
+ReactDOM.render(<App />, document.getElementById('errTrackerId'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
